e18f97fa1b51881ee0e7d87f27297b9fcfc4c472
pk <pipalpurus@gmail.com>
2021-06-14 16:53:31 +0545

    employee schema, module, controller and service added

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
+
+@Module({
+  imports: [
+    EmployeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/hrm'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
